bc184a5c94329dab2d1cfdf9cec94973c2548667
Baptiste Brullard <baptiste.brullard9@etu.univ-lorraine.fr>
2021-03-08 19:53:42 +0100

    part 2 exo 1

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,13 +1,38 @@
 import loader from './photoloader.js';
-import {config} from "./config.js";
+import { config } from "./config.js";
+import lightbox_ui from "./lightbox_ui.js"
+
+var vignette = null;
+/**
+ * Récupération des informations de la photo cliqué
+ */
+const load = (node) => {
+    vignette = node.parentNode;
+    return loader.loadRessource(config.root + node.getAttribute("data-uri"))
+}
 
 /**
-* Récupération des informations de la photo cliqué
-*/
-const load = (node) => {
-	return loader.loadRessource(config.root + node.getAttribute("data-uri"));
+ * On récupère la vignette précédente
+ */
+const prev = () => {
+    if (vignette.previousElementSibling === null)
+        return load(vignette.parentNode.lastElementChild.firstElementChild).then(lightbox_ui.display_lightbox)
+    else
+        return load(vignette.previousElementSibling.firstElementChild).then(lightbox_ui.display_lightbox)
+}
+
+/**
+ * On récupère la vignette suivante
+ */
+const next = () => {
+    if (vignette.nextElementSibling === null)
+        return load(vignette.parentNode.firstElementChild.firstElementChild).then(lightbox_ui.display_lightbox)
+    else
+        return load(vignette.nextElementSibling.firstElementChild).then(lightbox_ui.display_lightbox)
 }
 
 export default {
-	load
+    load,
+    prev,
+    next
 }
--- js/lightbox_ui.js
+++ js/lightbox_ui.js
@@ -1,30 +1,33 @@
-import loader from './photoloader.js';
-import {config} from "./config.js";
+import { config } from "./config.js";
+import lightbox from "./lightbox.js"
+
 
 const display_lightbox = (data) => {
-	document.querySelector('#lightbox_title').innerHTML = data.photo.titre;
-	document.querySelector('#lightbox_full_img').src = config.root + data.photo.url.href;
-	document.querySelector('#desc').innerHTML = data.photo.descr;
-	document.querySelector('#taille').innerHTML = data.photo.width + "x" + data.photo.height;
-	document.querySelector('#type').innerHTML = data.photo.type;
-	document.querySelector('#octet').innerHTML = data.photo.size;
-	show();
+    document.querySelector('#lightbox_title').innerHTML = data.photo.titre;
+    document.querySelector('#lightbox_full_img').src = config.root + data.photo.url.href;
+    document.querySelector('#desc').innerHTML = data.photo.descr;
+    document.querySelector('#taille').innerHTML = data.photo.width + "x" + data.photo.height;
+    document.querySelector('#type').innerHTML = data.photo.type;
+    document.querySelector('#octet').innerHTML = data.photo.size;
+    document.querySelector('#lightbox_previous').addEventListener('click', lightbox.prev);
+    document.querySelector('#lightbox_next').addEventListener('click', lightbox.next);
+    show();
 }
 
 const show = () => {
-	let lightbox = document.querySelector('#lightbox_container');
-	lightbox.classList.add('lightbox_container--visible');
-	lightbox.classList.remove('lightbox_container--hidden');
+    let lightbox = document.querySelector('#lightbox_container');
+    lightbox.classList.add('lightbox_container--visible');
+    lightbox.classList.remove('lightbox_container--hidden');
 }
 
 const hide = () => {
-	let lightbox = document.querySelector('#lightbox_container');
-	lightbox.classList.remove('lightbox_container--visible');
-	lightbox.classList.add('lightbox_container--hidden');
+    let lightbox = document.querySelector('#lightbox_container');
+    lightbox.classList.remove('lightbox_container--visible');
+    lightbox.classList.add('lightbox_container--hidden');
 }
 
 export default {
-	display_lightbox,
-	show,
-	hide
+    display_lightbox,
+    show,
+    hide
 }

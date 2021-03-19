ed69f03a7272163fc9a31c12a1264355637b26c0
Baptiste Brullard <baptiste.brullard9@etu.univ-lorraine.fr>
2021-03-19 12:05:27 +0100

    responsive pt

--- js/photoloader.js
+++ js/photoloader.js
@@ -1,27 +1,27 @@
 /**
-* Erreur lors d'un fetch vers l'API
-*/
+ * Erreur lors d'un fetch vers l'API
+ */
 const fetchError = (error) => {
-	console.log("Erreur lors de la tentative de contact de l'API :" + error);
+    console.log("Erreur lors de la tentative de contact de l'API :" + error);
 }
 
 /**
-* Conversion en JSON de la réponse de l'API
-*/
+ * Conversion en JSON de la réponse de l'API
+ */
 const conversionJson = (reponse) => {
-	if(reponse.ok) return reponse.json();
-	return Promise.reject(new Error(reponse.statusText));
+    if (reponse.ok) return reponse.json();
+    return Promise.reject(new Error(reponse.statusText));
 }
 
 /**
-* Méthode pour récupérer les informations sur la galerie
-*/
+ * Méthode pour récupérer les informations sur la galerie
+ */
 const loadRessource = (uri) => {
-	return fetch(uri, {
-		credentials: 'include'
-	}).then(conversionJson).catch(fetchError);	
+    return fetch(uri, {
+        credentials: 'include'
+    }).then(conversionJson).catch(fetchError);
 }
 
 export default {
-	loadRessource
+    loadRessource
 }
